b070dd3ec10661fdf403c2fd7a84e92b16a7ebc9
sseusti <stepan.s7ep@gmail.com>
2026-03-10 05:24:17 +0300

    feat: initialize http client in weather client

--- internal/client/weather.go
+++ internal/client/weather.go
@@ -1,16 +1,22 @@
 package client
 
-import "time"
+import (
+	"net/http"
+	"time"
+)
 
 type WeatherClient struct {
 	baseURL string
 	timeout time.Duration
+	client  *http.Client
 }
 
 func NewWeatherClient(baseURL string, timeout time.Duration) *WeatherClient {
+	client := &http.Client{Timeout: timeout}
 	return &WeatherClient{
 		baseURL: baseURL,
 		timeout: timeout,
+		client:  client,
 	}
 }
 
@@ -20,4 +26,8 @@ func (c *WeatherClient) BaseURL() string {
 
 func (c *WeatherClient) Timeout() time.Duration {
 	return c.timeout
+}
+
+func (c *WeatherClient) HTTPClient() *http.Client {
+	return c.client
 }
