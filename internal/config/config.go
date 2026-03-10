d1757bf76bf4a1cdf4046757f4aa88ea4c782e5f
sseusti <stepan.s7ep@gmail.com>
2026-03-10 04:54:06 +0300

    feat: add weather api base url to config

--- internal/config/config.go
+++ internal/config/config.go
@@ -3,13 +3,19 @@ package config
 import "os"
 
 type Config struct {
-	Port string
+	Port              string
+	WeatherAPIBaseURL string
 }
 
 func Load() *Config {
-	port := os.Getenv("PORT")
+	port := os.Getenv("HTTP_PORT")
 	if port == "" {
 		port = "8080"
+	}
+
+	weatherAPIBaseURL := os.Getenv("WEATHER_API_BASE_URL")
+	if weatherAPIBaseURL == "" {
+		weatherAPIBaseURL = "https://api.open-meteo.com"
 	}
 
 	return &Config{
